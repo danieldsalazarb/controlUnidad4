a5512b8846691738b1520f681b4681f2266cfbe8
danieldsalazarb <danieldsalazarb@gmail.com>
2023-02-17 12:29:27 +0100

    Initial commit

---
+++ src/main/java/Sueldo.java
@@ -0,0 +1,24 @@
+public class Sueldo {
+
+    public static double ej9(char tipo, double sueldo, int antiguedad) {
+        double incremento;
+        switch(tipo) {
+            case 'E': incremento=sueldo*1.0;
+            case 'N': incremento=sueldo*0.5;
+            default: incremento=0.0;
+        }
+        if (antiguedad>24)
+            incremento=incremento*antiguedad/10;
+        else
+        if (antiguedad>12)
+            incremento=incremento*antiguedad/20;
+        else
+            incremento=incremento*antiguedad/30;
+        return sueldo+incremento; // línea donde establecer un punto de ruptura, breakpoint
+    }
+
+
+    public static void main(String[] args) {
+        ej9( 'N',1000, 14);
+    }
+}
---
+++ src/test/java/SueldoTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class SueldoTest {
+
+    @Test
+    public void tes
+}
